d0a936fc7c43270805dc78c051b75258fa6a8ac0
almasumofficial98-dev <almasum.official.98@gmail.com>
2026-03-02 17:19:06 +0530

    feat: relocated app to /merge-pdfs path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
-import Merge from "./pages/Merge";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Merge from './pages/Merge';
 
 export default function App() {
-  return <Merge />;
+  return (
+    <Router>
+      <Routes>
+        {/* Automatically sends users from "/" to "/merge-pdfs" */}
+        <Route path="/" element={<Navigate to="/merge-pdfs" replace />} />
+        
+        {/* Your PDF tool now officially lives on this path */}
+        <Route path="/merge-pdfs" element={<Merge />} />
+      </Routes>
+    </Router>
+  );
 }
