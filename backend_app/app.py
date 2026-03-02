9a037da8e486662c7448716f02310734464dd03f
almasumofficial98-dev <almasum.official.98@gmail.com>
2026-03-02 16:28:59 +0530

    fix: added sys path for vercel python imports

--- backend_app/app.py
+++ backend_app/app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# 🌟 VERCEL FIX: Force Python to look in the root folder for imports
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from flask import Flask
 from flask_cors import CORS
 
